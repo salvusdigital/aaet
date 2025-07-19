086feaf3ab6d8454d92eb9dda8b57fdad8b8c994
HERMESBLACQK <omotosolilhenxy@gmail.com>
2025-07-19 20:53:13 +0100

    deleted backend files

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-
-routes.use('/api/menu', require('./menu'));
-
-routes.use('/api/admin', require('./admin'));
-
-// routes.use('/user', require('./user'));
-
-module.exports = routes;
